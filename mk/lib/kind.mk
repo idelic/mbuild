e28072b22f578a717ee4fcf14491a667f8a644a7
Matias Atria <matria@gmail.com>
2021-07-12 01:02:16 -0400

    Include kind definition files.

--- mk/lib/kind.mk
+++ mk/lib/kind.mk
@@ -1,4 +1,7 @@
 
-MK_ALL_KINDS = lib exe data config doc man
+MK_ALL_KINDS = lib exe 
+#TODO: data config doc man
 
 mk-kind-from-name = $(word 1,$(subst ., ,$1))
+
+include $(patsubst %,$(mk.mbuild.dir)/kind/%.mk,$(MK_ALL_KINDS))
