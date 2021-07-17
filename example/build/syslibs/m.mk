080a59ffa3a393d94cc8c0f54a4441fdb65327b9
Matias Atria <matria@gmail.com>
2021-07-17 17:24:32 -0400

    New files, to test third party libraries.

---
+++ example/build/syslibs/m.mk
@@ -0,0 +1,4 @@
+
+lib.m.name := m
+lib.m.pull-ldflags := -Wl,-rpath,/FOO
+lib.m.link := -lm
---
+++ example/build/syslibs/pthread.mk
@@ -0,0 +1,4 @@
+
+lib.pthread.name := pthread
+lib.pthread.pull-cxxflags := -pthread
+lib.pthread.link := -pthread
