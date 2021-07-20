bd312322174dc6875afc3cec86ab70f2d9556afc
Matias Atria <matria@gmail.com>
2021-07-19 23:21:03 -0400

    Removed pointless comments

--- mk/lib/top.mk
+++ mk/lib/top.mk
@@ -6,24 +6,13 @@ MK_TOP_MK_ := $(lastword $(MAKEFILE_LIST))
 # This is the directory where the mbuild files live
 mk.mbuild.dir := $(patsubst %/,%,$(dir $(MK_TOP_MK_)))
 
-# Our core goodies
 include $(mk.mbuild.dir)/functions.mk
-
-# Load all our configuration files
 include $(mk.mbuild.dir)/config.mk
-
 include $(mk.mbuild.dir)/origin.mk
-
-# Determine the build mode, make flags, etc
 include $(mk.mbuild.dir)/make.mk
-
-# Other facilities
 include $(mk.mbuild.dir)/terminal.mk
 include $(mk.mbuild.dir)/include.mk
 #include $(mk.mbuild.dir)/install.mk
-#include $(mk.mbuild.dir)/clean.mk
-
-# Get the target machinery going
 include $(mk.mbuild.dir)/kind.mk
 include $(mk.mbuild.dir)/targets.mk
 include $(mk.mbuild.dir)/help.mk
