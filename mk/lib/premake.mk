1b71a7eb6d0d204f4e1115318267cd13fb8e2d45
Matias Atria <matria@gmail.com>
2021-07-21 00:56:33 -0400

    Fix to make it possible to symlink to the top level GNUmakefile from sub-directories.

--- mk/lib/premake.mk
+++ mk/lib/premake.mk
@@ -1,6 +1,7 @@
+
 THIS_FILE := $(realpath $(lastword $(MAKEFILE_LIST)))
 ROOT_DIR := $(dir $(abspath $(THIS_FILE)))
-TOP_MAKEFILE := $(firstword $(MAKEFILE_LIST))
+TOP_MAKEFILE := $(realpath $(firstword $(MAKEFILE_LIST)))
 
 .NOTPARALLEL:
 
